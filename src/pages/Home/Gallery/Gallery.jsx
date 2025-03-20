e4602575dbc4dd4bdf5225e49ce14344aa03b0a9
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-03-21 02:22:11 +0600

    gallery section running

--- src/pages/Home/Gallery/Gallery.jsx
+++ src/pages/Home/Gallery/Gallery.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import image1 from "../../../assets/gallery/image1.png"
+import image2 from "../../../assets/gallery/image2.png"
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
+    <div className='md:w-[80%] mx-auto my-28'>
+      <div className='mb-16'>
+        <h1 className='text-5xl font-bold text-center'>Our Gallery</h1>
+      </div>
+      <div>
+        <div>
+          <img src={image1} alt="" />
+        </div>
+        <div></div>
+      </div>
+    </div>
   )
 }
 
